772c1de9b123092ac314c3ada5dcd97c812ec82c
PSH-2005 <piyush.sh.btech@gmail.com>
2024-10-21 10:16:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Auth0Provider
+    domain="dev-eiozg6igek2vkakw.us.auth0.com"
+    clientId="RW5CBddZ9acLsEiipU9DcNFxI8Idxo50"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
+  </Auth0Provider>,
 )
